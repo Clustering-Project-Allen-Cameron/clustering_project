98e27f747120882c8e9afa3709641ba9f0a2e298
Cameron Taylor <taylor.cameron.g@gmail.com>
2020-05-06 16:03:48 -0500

    'Continue working on outliers and other prep'

--- cameron_project/wrangle_zillow.py
+++ cameron_project/wrangle_zillow.py
@@ -43,6 +43,74 @@ def null_cols_info(df):
     df2.rename(columns={'index':'num_rows'}, inplace=True)
     return df2
 
+def how_many_outliers(s, k):
+    '''
+    Given a series and a cutoff value k, returns the count of all values outside 
+    the bounds of upper and lower outliers.
+    '''
+    q1, q3 = s.quantile([.25, .75])
+    iqr = q3 - q1
+    upper_bound = q3 + k * iqr
+    lower_bound = q1 - k * iqr
+    outliers_high = [num for num in s if num > upper_bound]
+    outliers_low = [num for num in s if num < lower_bound]
+    return len(outliers_high) + len(outliers_low)
+
+def drop_outliers(s, k):
+    '''
+    Given a series and a cutoff value k, drops all values outside the bounds of 
+    upper outliers and returns the series.
+    '''
+    q1, q3 = s.quantile([.25, .75])
+    iqr = q3 - q1
+    upper_bound = q3 + k * iqr
+    # lower_bound = q1 - k * iqr
+    outliers_high = pd.Series([num for num in s if num > upper_bound])
+    # outliers_low = pd.Series([num for num in s if num < lower_bound])
+    
+    s = s[s < outliers_high.min()]
+    # s = s[s > outliers_low.max()]
+
+    return s
+
+def extra_clean(df):
+    #Drop outliers
+    # q1 = numbers.quantile(.25)
+    # q3 = numbers.quantile(.75)
+    # iqr = q3 - q1    
+    # upper_bound = q3 + 1.5 * iqr
+    # lower_bound = q1 - 1.5 * iqr
+
+    # df.drop(df.bathroomcnt.nlargest(1).index.tolist(), 
+    #     inplace=True)
+    # df.drop(df.calculatedfinishedsquarefeet.nlargest(3).index.tolist(), 
+    #     inplace=True)
+    # df.drop(df.structuretaxvaluedollarcnt.nlargest(1).index.tolist(), 
+    #     inplace=True)
+    # df.drop(df.landtaxvaluedollarcnt.nlargest(4).index.tolist(), 
+    #     inplace=True)
+    # df.drop(df.regionidzip.nlargest(12).index.tolist(), 
+    #     inplace=True)
+    # df.drop(df.lotsizesquarefeet.nlargest(13).index.tolist(), 
+    #     inplace=True)
+    # df.drop(df.logerror.nsmallest(2).index.tolist(), 
+    #     inplace=True)
+
+    #Impute nulls with median
+    df.calculatedfinishedsquarefeet.fillna(
+        df.calculatedfinishedsquarefeet.median(), inplace=True)
+    df.lotsizesquarefeet.fillna(df.lotsizesquarefeet.median(),
+                            inplace=True)
+    df.regionidcity.fillna(df.regionidcity.mode()[0], inplace=True)
+    df.regionidzip.fillna(df.regionidzip.mode()[0], inplace=True)
+    df.yearbuilt.fillna(df.yearbuilt.median(), inplace=True)
+    df.structuretaxvaluedollarcnt.fillna(
+        df.structuretaxvaluedollarcnt.median(), inplace=True)
+    df.roomcnt.replace(0,df.roomcnt.mode()[0], inplace=True)
+    df.set_index('parcelid', inplace=True)
+    df.dropna(inplace=True)
+    return df
+
 def handle_missing_values(df, req_column = .7, req_row = .6):
     single_unit = [261]
     df = df[df.propertylandusetypeid.isin(single_unit)] 
@@ -58,34 +126,6 @@ def handle_missing_values(df, req_column = .7, req_row = .6):
                     'propertylandusedesc','assessmentyear',
                     'transactiondate',
                     'censustractandblock','regionidcounty'])
-    return df
 
-def extra_clean(df):
-    #Drop outliers
-    df.drop(df.bathroomcnt.nlargest(1).index.tolist(), 
-    inplace=True)
-    df.drop(df.calculatedfinishedsquarefeet.nlargest(3).index.tolist(), 
-    inplace=True)
-    df.drop(df.structuretaxvaluedollarcnt.nlargest(1).index.tolist(), 
-    inplace=True)
-    df.drop(df.landtaxvaluedollarcnt.nlargest(4).index.tolist(), 
-    inplace=True)
-    df.drop(df.regionidzip.nlargest(12).index.tolist(), 
-    inplace=True)
-    df.drop(df.lotsizesquarefeet.nlargest(13).index.tolist(), 
-    inplace=True)
-
-    #Impute nulls with median
-    df.calculatedfinishedsquarefeet.fillna(
-        df.calculatedfinishedsquarefeet.median(), inplace=True)
-    df.lotsizesquarefeet.fillna(df.lotsizesquarefeet.median(),
-                            inplace=True)
-    df.regionidcity.fillna(df.regionidcity.median(), inplace=True)
-    df.regionidzip.fillna(df.regionidzip.median(), inplace=True)
-    df.yearbuilt.fillna(df.yearbuilt.median(), inplace=True)
-    df.structuretaxvaluedollarcnt.fillna(
-        df.structuretaxvaluedollarcnt.median(), inplace=True)
-    df.roomcnt.replace(0,df.roomcnt.median(), inplace=True)
-    df.set_index('parcelid', inplace=True)
-    df.dropna(inplace=True)
+    df = extra_clean(df)
     return df
